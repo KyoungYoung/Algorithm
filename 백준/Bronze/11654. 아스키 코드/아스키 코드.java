b82fbcb78abd4fdde543d5ed9b0cb4aa83fcba32
김경영 <92295755+KyoungYoung@users.noreply.github.com>
2024-01-05 00:35:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       String line = sc.next();
+        char alphabet = line.charAt(0);
+        int res = (int) alphabet;
+        System.out.println(res);
+    }
+}
