26ade5b36bc358d9ada3961877118396109ee2c8
김경영 <92295755+KyoungYoung@users.noreply.github.com>
2024-01-04 18:26:34 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,21 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i = 1; i < 10; i++){
+//            System.out.println(n + " * " + i + " = " +n*i);
+//        }
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
 
+        StringBuilder sb = new StringBuilder();
+
         if ((score >= 90 && score <= 100)) {
-            System.out.println("A");
+            sb.append("A");
         } else if ((score >= 80 && score < 90)) {
-            System.out.println("B");
+            sb.append("B");
         }else if ((score >= 70 && score < 80)) {
-            System.out.println("C");
+            sb.append("C");
         }else if ((score >= 60 && score < 70)) {
-            System.out.println("D");
+            sb.append("D");
         }else {
-            System.out.println("F");
+            sb.append("F");
         }
-        
+
+        System.out.println(sb);
+
+
+
+
     }
-}
+    }
+
